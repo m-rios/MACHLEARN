0e7130011702dc4d52649e5834f465586ba00f86
Mario <mrm994@gmail.com>
2017-12-13 12:43:58 +0100

    Integration with stockfish finished

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 from stock_agent import StockAgent
 from dojo import Dojo
+import os
 
 def main():
     
-    dojo = Dojo(StockAgent, StockAgent, [])
-    print dojo.train()
+    dojo = Dojo(StockAgent, StockAgent, 'n7/5k2/8/8/8/8/8/3R3K w - - 0 1')
+    (history, result) = dojo.play()
+    
+    mydir = 'results/svg/'
+    filelist = [ f for f in os.listdir(mydir) if f.endswith(".svg") ]
+    for f in filelist:
+        os.remove(os.path.join(mydir, f))
 
+    for i in range(len(history)-1):
+        with open('/Users/mario/Developer/MACHLEARN/results/svg/move_{}.svg'.format(i), 'w') as f:
+            f.write(history[i])
+    print(result)
 
 if __name__ == '__main__':
     main()
