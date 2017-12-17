6f4839f5056cdb198cc04a04aaf72e9abf75f765
tmnvanderberg@gmail.com <tmnvanderberg@gmail.com>
2017-12-17 16:06:37 +0100

    might be working.

--- src/multilayer.py
+++ src/multilayer.py
@@ -53,45 +53,63 @@ class MultiLayerPerceptron:
 
         """ train to produce score given board """
 
-        self.board = fromFen(fenboard)                  # first layer input
+        self.board = npy.transpose(npy.asmatrix(fromFen(fenboard)))                  
         self.targ = score                               # score from stockfish
         
-        self.predictScore()                                  # current score
-        self.calcDelta()                                     # errors
-        self.updateWeights()                                 # update rule
+        self.predictScore()                             # current score
+        self.calcDelta()                                # delta errors 
+        self.updateDeltaWeights()                       # update rule 
+        self.updateWeights()                            # apply updates
 
     def calcDelta(self):
 
         """ calculate delta's, back-propagating """
+
+        print("calculating delta values...")
        
         self.layers[-1].delta = self.targ - self.layers[-1].output
         for n in range(0, len(self.layers) - 1):
             leftsum  = self.layers[n + 1].w * self.layers[n + 1].delta
             rightsum = npy.transpose(self.layers[n].input) * self.layers[n].w
-            rightsum = npy.transpose(rightsum)
             self.layers[n].delta = npy.multiply(leftsum,
-                    self.layers[n].actiFun(rightsum))
+                    self.layers[n].actiFun(npy.transpose(rightsum)))
 
     def predictScore(self):
 
         """ get output, forward-propagating """ 
 
+        print("predicting...")
+
         self.layers[0].input  = self.board 
-        self.layers[0].output = self.layers[0].actiFun(npy.dot(self.layers[0].input,
-            self.layers[0].w))
+        self.layers[0].output = npy.transpose(self.layers[0].actiFun(npy.transpose(self.layers[0].input) *
+                self.layers[0].w))
 
         for n in range(1, len(self.layers)):
             self.layers[n].input  = self.layers[n - 1].output 
-            self.layers[n].output = self.layers[0].actiFun(npy.dot(self.layers[n].input,
-                self.layers[n].w))
+            self.layers[n].output = npy.transpose(self.layers[0].actiFun(npy.transpose(self.layers[n].input)
+                    * self.layers[n].w))
+
+    def updateDeltaWeights(self):
+
+        """ update deltaw values """
+
+        print("updating deltaw values...")
+        
+        for layer in self.layers: 
+            if layer.input.shape == (1,1):
+                layer.input = npy.asscaler(layer.input)
+            layer.deltaw += self.eta * layer.input * npy.transpose(layer.delta)
 
     def updateWeights(self):
 
-        """ yeah this updates the weights how did u know """
-        
-        for n in range(0, len(self.layers)):
-            self.layers[n].deltaw = self.eta * self.layers[n].delta * npy.transpose(self.layers[n].input)
-        # apply deltaw here
+        """ apply deltaw to update w """
+
+        print("updating weights...")
+        for layer in self.layers:
+            layer.w += layer.deltaw
+            layer.delta = npy.asmatrix(npy.zeros((layer.inputsize,
+            layer.outputsize)))
+
 
 """ ------------------------------------------------------------------------"""
 if __name__ == "__main__":
@@ -101,6 +119,6 @@ if __name__ == "__main__":
     numlayers = 2
     inputsizes = [64 * 4, 20] 
     outputsizes = [20, 1]
-    
-    mlp = MultiLayerPerceptron(0.9, numlayers, inputsizes, outputsizes)
+    eta = 1/5 
+    mlp = MultiLayerPerceptron(eta, numlayers, inputsizes, outputsizes)
     mlp.train(test, 5)
