52de883033272c5502119192198c787126cdcea6
M.Rios.Munoz@student.rug.nl <s3485781@peregrine.hpc.rug.nl>
2018-01-12 22:15:33 +0100

    Modified data generators to run in hpc

--- src/generate_labels.py
+++ src/generate_labels.py
@@ -2,11 +2,12 @@ import chess
 import chess.uci
 
 handler = chess.uci.InfoHandler()
-engine = chess.uci.popen_engine('stockfish') #give correct address of your engine here
+engine = chess.uci.popen_engine('/home/s3485781/stockfish-8-linux/Linux/stockfish_8_x64') #give correct address of your engine here
 engine.info_handlers.append(handler)
 
 with open('../data/fen_games') as f:
     with open('../data/labels','w+') as label_file:
+        c = 0
         for fen in f:
             board = chess.Board(fen)
             engine.position(board)
@@ -20,4 +21,7 @@ with open('../data/fen_games') as f:
             else:
                 score = 0
             label_file.write('{}\n'.format(score))
+            if not (c % 100):
+                label_file.flush()
+            c += 1
 
