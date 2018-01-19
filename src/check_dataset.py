a388bc89475e953494c665821c0a5d315ac96580
M.Rios.Munoz@student.rug.nl <s3485781@peregrine.hpc.rug.nl>
2018-01-20 00:57:08 +0100

    Added final clean dataset

--- src/check_dataset.py
+++ src/check_dataset.py
@@ -1,6 +1,5 @@
 import chess
 
-fens = []
 n = 0
 with open('../data/fen_games') as f:
     with open('../data/fen_games_2','w+') as f2:
@@ -8,7 +7,6 @@ with open('../data/fen_games') as f:
             board = chess.Board(fen)
             if board.is_valid() and not board.is_game_over():
                 f2.write(fen)
-                f2.flush()
                 n += 1
-                if not (n % 100):
+                if not (n % 1000):
                     f2.flush()
