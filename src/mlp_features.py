40d2a4a050fe6b43e1dcf54ff5d7bc244dde414d
swaraj <swarajdalmia.bitspilani@gmail.com>
2018-01-27 11:49:03 +0100

    Include the turn in the network.

--- src/mlp_features.py
+++ src/mlp_features.py
@@ -3,12 +3,15 @@ import tensorflow as tf
 class MlpFeatures( object ):
     def __init__(self):
         
-        self.n_general = 3; self.n_piece_c = 12; self.n_square_c = 128
+        self.n_general = 5; self.n_piece_c = 14; self.n_square_c = 130
         self.n_input = self.n_general + self.n_piece_c + self.n_square_c
         self.n_hidden_2 = 9
         self.out = 3
 
         self.X = tf.placeholder("float", shape=[None, self.n_input])
+        self.turn = tf.gather(self.X, [64*4, 64*4+1], axis = 1)
+        self.X = tf.gather(self.X, [i for i in range(64*4)], axis = 1)
+
 
         self.weights = {
             'general': tf.Variable(tf.random_normal([self.n_general, self.n_general])),
@@ -25,14 +28,22 @@ class MlpFeatures( object ):
 
         # Locally connected layers
         general_i = tf.gather(self.X,tf.convert_to_tensor(list(range(self.n_general)), dtype=tf.int32),axis=1)
+        general_i = tf.concat([general_i,self.turn],1)
+
         piece_i = tf.gather(self.X,tf.convert_to_tensor(list(range(self.n_general,self.n_general+self.n_piece_c)), dtype=tf.int32), axis=1)
+        piece_i = tf.concat([piece_i,self.turn],1)
+
         square_i = tf.gather(self.X,tf.convert_to_tensor(list(range(self.n_general+self.n_piece_c, self.n_general + self.n_piece_c + self.n_square_c)), dtype=tf.int32), axis=1)
+        square_i = tf.concat([square_i,self.turn],1)
+
+        
         general = tf.matmul(general_i, self.weights['general'])
         piece_c = tf.matmul(piece_i, self.weights['piece_c'])
         square_c = tf.matmul(square_i, self.weights['square_c'])
-        hidden_1 = tf.nn.relu(tf.add(tf.concat([general, piece_c, square_c], 1), self.biases['b1']))
+
+        hidden_1 = tf.nn.tanhtf.add(tf.concat([general, piece_c, square_c], 1), self.biases['b1']))
         # Fully connected layer
-        hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, self.weights['hidden_2']), self.biases['b2']))
+        hidden_2 = tf.nn.tanh(tf.add(tf.matmul(hidden_1, self.weights['hidden_2']), self.biases['b2']))
         # Output layer
         self.last_layer = tf.add(tf.matmul(hidden_2, self.weights['out']), self.biases['out'])
         self.class_prob = tf.nn.softmax(self.last_layer)
